6efa80cc186b6e6354a4aef6cf551d9c7b152cb8
ikriat <ilyes.kriat@outlook.com>
2021-01-05 10:17:36 +0100

    End of course Fix Issue 1

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -51,7 +51,7 @@ namespace API.Extensions
                     var pgHost = pgHostPort.Split(":")[0];
                     var pgPort = pgHostPort.Split(":")[1];
 
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL=true;SslMode=Require;" ;
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SslMode=Require;" ;
 
 
                     Console.WriteLine("Connection String = " + connStr);
